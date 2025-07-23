3ce25debffba53aeae64268505e806ac81a298a7
faisalbhai90 <nahidfaisalrony@gmail.com>
2025-07-24 01:32:20 +0600

    Update awm_patch.cs

--- awm_patch.cs
+++ awm_patch.cs
@@ -21,21 +21,15 @@ public class AWM_Patch
 
             var result = await mem.AoBScan("0A D7 A3 3D 00 00 00 00 00 00 5C 43 00 00 90 42 00 00 B4 42 96 00 00 00 00 00 00 00 00 00 00 3F 00 00 80 3E 00 00 00 00 04 00 00 00 00 00 80 3F 00 00 20 41 00 00 34 42 01 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 0A D7 23 3F 9A 99 99 3F 00 00 80 3F 00 00 00 00 00 00 80 3F 00 00 80 3F 00 00 80 3F 00 00 00 00 00 00 00 00 00 00 00 3F 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 00 00 80 3F 00 00 80 3F 00 00").ConfigureAwait(false);
 
-            PID.Text = $"📦 Found: {result.Count()} Match(es)";
+            
 
-            if (result.Count() == 0)
-            {
-                MessageBox.Show("❌ AWM value not found. Try in lobby or check pattern.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
+            if (result is IEnumerable<long> addresses)
+        {
+    var addressList = addresses.ToList();
 
-            if (result.Count() > 2)
-            {
-                MessageBox.Show("⚠️ Too many results found. Not safe to patch.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+    if (addressList.Count > 0 && addressList.Count < 2)
 
-            foreach (long address in result)
+            foreach (var num in addressList)
             {
                 mem.WriteMemory(num.ToString("X"), "bytes", "0A D7 A3 3D 00 00 00 00 00 00 5C 43 00 00 90 42 00 00 B4 42 96 00 00 00 00 00 00 00 EC 51 B8 3D 8F C2 F5 3C 00 00 00 00 04 00 00 00 00 00 80 3F 00 00 20 41 00 00 34 42 01 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 0A D7 23 3F 9A 99 99 3F 00 00 80 3F 00 00 00 00 00 00 80 3F 00 00 80 3F 00 00 80 3F 00 00 00 00 00 00 00 00 00 00 00 3F 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 00 00 80 3F 00 00 80 3F 00 00", string.Empty, null);
             }
