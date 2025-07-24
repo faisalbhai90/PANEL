f91552dd830205ea471d94a1240ae32f1b0adec0
faisalbhai90 <nahidfaisalrony@gmail.com>
2025-07-24 15:56:19 +0600

    Create m82b_patch.cs

---
+++ m82b_patch.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public static class M82B_Patch
+{
+    public static async Task Run(dynamic mem, Label PID, bool isMuted = false)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        if (!isMuted)
+            Console.Beep(100, 200);
+
+        PID.Text = "ᴀᴘᴘʟʏɪɴɢ";
+
+        Int32 proc = Process.GetProcessesByName("HD-Player")[0].Id;
+        mem.OpenProcess(proc);
+
+        string searchPattern = "99 3E 00 00 00 00 00 00 5C 43 00 00 28 42 00 00 B4 42 78 00 00 00 00 00 00 00 9A 99 19 3F 00 00 80 3E 00 00 00 00 04 00 00 00 00 00 80 3F 00 00 20 41 00 00 34 42 01 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 9A 99 19 3F CD CC 8C 3F 00 00 80 3F 00 00 00 00 66 66 66 3F 00 00 80 3F 00 00 80 3F 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 00 00 80 3F 00 00 80 3F 00 00 00 00 01 00 00";
+
+        var result = await mem.AoBScan(searchPattern);
+
+        if (result.Count() != 0 && result.Count() < 2)
+        {
+            foreach (var CurrentAddress in result)
+            {
+                Int64 address10thByte = CurrentAddress + 0L;
+                Int64 address11thByte = CurrentAddress + 4L;
+
+                mem.WriteMemory(address10thByte.ToString("X"), "bytes", "99 3E 00 00 00 00 00 00 5C 43 00 00 28 42 00 00 B4 42 78 00 00 00 00 00 00 00 9A 99 19 1B 00 00 80 10 00 00 00 00 04 00 00 00 00 00 80 3F 00 00 20 41 00 00 34 42 01 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 9A 99 19 3F CD CC 8C 3F 00 00 80 3F 00 00 00 00 66 66 66 3F 00 00 80 3F 00 00 80 3F 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 80 3F 00 00 80 3F 00 00 80 3F 00 00 00 00 01 00 00");
+            }
+
+            stopwatch.Stop();
+            double elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+
+            if (!isMuted)
+                Console.Beep(200, 300);
+
+            PID.Text = $"M82B ꜰᴀꜱᴛ-ꜱᴡɪᴛᴄʜ=ᴏɴ,ᴛɪᴍᴇ: {elapsedSeconds:F2} Seconds";
+        }
+        else if (result.Count() >= 2)
+        {
+            MessageBox.Show("THIS CODE IS PATCHED.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        else
+        {
+            PID.Text = "ERROR";
+
+            if (!isMuted)
+            {
+                Console.Beep(2000, 400);
+            }
+        }
+    }
+}
